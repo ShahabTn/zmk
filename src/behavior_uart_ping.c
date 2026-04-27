57637d43cc3bde34ce67eac19a00a4c8cbffdef9
Shahab Tn <Shahab.tanhaei@outlook.com>
2026-04-27 18:21:25 +0200

    Fix UART ping behavior source for out-of-tree build

--- src/behavior_uart_ping.c
+++ src/behavior_uart_ping.c
@@ -4,10 +4,11 @@
 #include <zephyr/drivers/uart.h>
 #include <zephyr/logging/log.h>
 
-#include <drivers/behavior.h>
 #include <zmk/behavior.h>
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
+#if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
 
 static int uart_ping_keymap_binding_pressed(struct zmk_behavior_binding *binding,
                                             struct zmk_behavior_binding_event event) {
@@ -46,12 +47,12 @@ static const struct behavior_driver_api uart_ping_driver_api = {
     .binding_released = uart_ping_keymap_binding_released,
 };
 
-DT_INST_FOREACH_STATUS_OKAY(BEHAVIOR_DT_INST_DEFINE)
-
 static int uart_ping_init(const struct device *dev) { return 0; }
 
-#define UART_PING_INST(n)                                                                              \
-    BEHAVIOR_DT_INST_DEFINE(n, uart_ping_init, NULL, NULL, NULL, POST_KERNEL,                         \
-                            CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &uart_ping_driver_api);
+#define UART_PING_INST(n)                                                                   \
+    BEHAVIOR_DT_INST_DEFINE(n, uart_ping_init, NULL, NULL, NULL, POST_KERNEL,              \
+                            CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &uart_ping_driver_api)
 
 DT_INST_FOREACH_STATUS_OKAY(UART_PING_INST)
+
+#endif /* DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT) */
